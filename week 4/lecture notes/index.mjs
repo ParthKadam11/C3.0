426b5b9d17136177ccf44180f62c65657fce333b
Parth Ganpat Kadam <parthkadamalt@gmail.com>
2025-07-22 21:33:05 +0530

    Lecture Notes project (Node.js + Express)
    Added complete Node.js + Express lecture notes project from Week 4.
    Includes routing, middleware, and REST API basics.

---
+++ week 4/lecture notes/index.mjs
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+console.log(chalk);
+console.log("Hello World");
+console.log(chalk.blue("Hello World Blue"));
+console.log(chalk.red.bold("Hello World Red"));
+console.log(chalk.green.underline("Hello World Green")); 
